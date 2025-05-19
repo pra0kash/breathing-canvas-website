51e6d164879970c6dab704f3f9d55b95dfa2f03d
pra0kash <sardaprakash96@gmail.com>
2025-05-20 00:14:03 +0530

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function handleBooking(event) {
+  event.preventDefault();
+  alert("Thank you for your booking request! We'll contact you soon.");
+  document.getElementById("booking-form").reset();
+}
